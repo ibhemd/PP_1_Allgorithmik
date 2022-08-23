f0e6013ffa59ec3f6b015c0ecc1418eebfeee79d
ibhemd <95880376+ibhemd@users.noreply.github.com>
2022-08-23 10:56:09 +0200

    Add files via upload

---
+++ MyGraph.java
@@ -0,0 +1,186 @@
+package vertexcover;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class MyGraph implements Graph {
+
+    public HashMap<Integer, LinkedList<Integer>> adjacencyList;
+
+    public MyGraph(String filename) throws FileNotFoundException {
+        adjacencyList = new HashMap<>();
+        Scanner scanner = new Scanner(new File("data/" + filename));
+        while (scanner.hasNext()) {
+            String s = scanner.nextLine();
+
+            int index = s.indexOf(" ");
+            if (index == -1) {
+                index = s.indexOf("\t");
+            }
+
+            Integer v = Integer.parseInt(s.substring(0,index));
+            Integer w = Integer.parseInt(s.substring(index+1).replaceAll("\\s",""));
+            addEdge(v,w);
+        }
+    }
+
+    public MyGraph(MyGraph g) {
+        this.adjacencyList = new HashMap<>();
+        for (Integer key : g.adjacencyList.keySet()) {
+            this.adjacencyList.put(key, new LinkedList<Integer>());
+            for (Integer i : g.adjacencyList.get(key)) {
+                this.adjacencyList.get(key).add(i);
+            }
+        }
+    }
+
+    @Override
+    public void addVertex(Integer v) {
+        this.adjacencyList.put(v, new LinkedList<Integer>());
+    }
+
+    @Override
+    public void addEdge(Integer v, Integer w) {
+        if (this.adjacencyList == null || !this.adjacencyList.containsKey(v)) {
+            addVertex(v);
+        }
+        if (!this.adjacencyList.containsKey(w)) {
+            addVertex(w);
+        }
+        this.adjacencyList.get(v).add(w);
+        this.adjacencyList.get(w).add(v);
+    }
+
+    @Override
+    public void deleteVertex(Integer v) {
+        if (this.adjacencyList.containsKey(v)) {
+            for (Integer key : adjacencyList.keySet()) {
+                deleteEdge(key, v);
+            }
+            this.adjacencyList.remove(v);
+        }
+    }
+
+    @Override
+    public void deleteEdge(Integer u, Integer v) {
+        if (this.adjacencyList.containsKey(u) && this.adjacencyList.containsKey(v)) {
+            this.adjacencyList.get(u).remove(v);
+            this.adjacencyList.get(v).remove(u);
+        }
+    }
+
+    @Override
+    public boolean contains(Integer v) {
+        if (this.adjacencyList.containsValue(v)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int degree(Integer v) {
+        if (this.adjacencyList.get(v) == null) {
+            return 0;
+        } else {
+            return this.adjacencyList.get(v).size();
+        }
+    }
+
+    @Override
+    public boolean adjacent(Integer v, Integer w) {
+        if(v.equals(w)) return false;
+        if(this.adjacencyList.get(v).contains(w) || this.adjacencyList.get(w).contains(v)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public MyGraph getCopy() {
+        return new MyGraph(this);
+    }
+
+    @Override
+    public Set<Integer> getNeighbors(Integer v) {
+        Set<Integer> res = new HashSet<Integer>();
+        for (Integer key : this.adjacencyList.keySet()) {
+            if (adjacent(key,v)) {
+                res.add(key);
+            }
+        }
+        return res;
+    }
+
+    @Override
+    public int size() {
+        return this.adjacencyList.size();
+    }
+
+    @Override
+    public int getEdgeCount() {
+        if (size() == 0) {
+            return 0;
+        } else {
+            int res = 0;
+            Set s = getVertices();
+            for (Object i : s) {
+                res += degree((Integer) i);
+            }
+            return res/2;
+        }
+    }
+
+    @Override
+    public Set<Integer> getVertices() {
+        Set<Integer> res = new HashSet<Integer>();
+        for (Integer key : this.adjacencyList.keySet()) {
+            res.add(key);
+        }
+        return res;
+    }
+
+    public String toString() {
+        return this.adjacencyList.toString();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        long begin = System.nanoTime();
+
+        String[] arr = new String[15];
+        //arr[0] = "bio-dmelamtx.sec";                          // |V|:  7393 || |E|:  25569 || k:      ->  2630
+        //arr[1] = "ca-sandi_authsmtx.sec";                     // |V|:    86 || |E|:    124 || k:      ->    38
+        //arr[2] = "inf-openflightsedges.sec";                  // |V|:  2939 || |E|:  30501 || k:      ->  1088
+        //arr[3] = "inf-powermtx.sec";                          // |V|:  4941 || |E|:   6594 || k:      ->  2203
+        //arr[4] = "inf-USAir97mtx.sec";                        // |V|:   332 || |E|:   2126 || k:      ->   149
+        //arr[5] = "outadjnoun_adjacency_adjacency.sec";        // |V|:   112 || |E|:    425 || k:      ->    59
+        //arr[6] = "outarenas-email.sec";                       // |V|:  1133 || |E|:   5451 || k:      ->   594
+        //arr[7] = "outarenas-jazz.sec";                        // |V|:   198 || |E|:   2742 || k:      ->   157
+        //arr[8] = "outcontiguous-usa.sec";                     // |V|:    49 || |E|:    107 || k:      ->    30
+        //arr[9] = "outdolphins.sec";                           // |V|:    62 || |E|:    159 || k:      ->    34
+        //arr[10] = "outmoreno_zebra_zebra.sec";                // |V|:    27 || |E|:    111 || k: 20   ->    20
+        //arr[11] = "outucidata-zachary.sec";                   // |V|:    34 || |E|:     78 || k: 14   ->    14
+        arr[12] = "sample.sec";                               // |V|:    17 || |E|:     22 || k: 10   ->    10
+        //arr[13] = "sample2.sec";                              // |V|:    12 || |E|:     12 || k:  4   ->     4
+        //arr[14] = "soc-brightkitemtx.sec";                    // |V|: 56739 || |E|: 212945 || k:      -> 21190
+
+        int i = 1;
+        for (String s : arr) {
+            if (s != null) {
+                System.out.println("");
+                System.out.print("File #" + i + ": " + s + " || ");
+                MyGraph sample = new MyGraph(s);
+                SearchTree.solve(sample);
+            }
+            i += 1;
+        }
+
+        long end = System.nanoTime();
+        long diff = end - begin;
+        
+        System.out.println("Duration: " + diff + "ns < 1 min. : " + (diff < (6*Math.pow(10,10))));
+    }
+
+}
---
+++ SearchTree.java
@@ -0,0 +1,109 @@
+package vertexcover;
+
+import java.io.FileNotFoundException;
+
+public class SearchTree {
+
+    static class Instance {
+        MyGraph G;
+        int k;
+
+        Instance(MyGraph g, int k) {
+            this.G = g;
+            this.k = k;
+        }
+    }
+
+    private static boolean solve(Instance i) {
+
+        if (i.k < 0) {
+            return false;
+        }
+        if (i.G.getEdgeCount() == 0) {
+            return true;
+        }
+
+        Integer u = null;
+        Integer v = null;
+
+        for (Integer x : i.G.getVertices()) {
+            if (i.G.degree(x) != 0) {
+                u = x;
+                v = i.G.getNeighbors(u).iterator().next();
+            }
+        }
+
+        removeSingletons(i.G);
+
+        Instance i1 = new Instance(i.G.getCopy(), i.k-1);
+        if (i.G.degree(u) == 1) {
+            removeDegOne(i);
+            int n = i1.G.getNeighbors(u).iterator().next();
+            i1.G.deleteVertex(n);
+        } else if (i.G.degree(v) > i.k) {
+            removeHighDeg(i1);
+        } else {
+                i1.G.deleteVertex(u);
+        }
+
+        Instance i2 = new Instance(i.G.getCopy(), i.k-1);
+        if (i.G.degree(v) == 1) {
+            removeDegOne(i);
+            int n = i2.G.getNeighbors(v).iterator().next();
+            i2.G.deleteVertex(n);
+        } else if (i.G.degree(v) > i.k) {
+            removeHighDeg(i2);
+        } else {
+            i2.G.deleteVertex(v);
+        }
+
+        if (solve(i1)) return true;
+        if (solve(i2)) return true;
+        return false;
+    }
+
+    private static void removeSingletons(MyGraph g) {
+        for (Integer key : g.getVertices()) {
+            if (g.degree(key) == 0) {
+                g.deleteVertex(key);
+            }
+        }
+    }
+
+    private static void removeDegOne(Instance i) {
+        for (int v = 0; v < i.G.size(); v++) {
+            if (i.G.degree(v) == 1) {
+                int u = i.G.getNeighbors(v).iterator().next();
+                i.G.deleteVertex(u);
+                i.k -= 1;
+                v += 1;
+            }
+        }
+    }
+
+    private static void removeHighDeg(Instance i) {
+        for (int v = 0; v < i.G.size(); v++) {
+            if (i.G.degree(v) > i.k) {
+                i.G.deleteVertex(v);
+                i.k -= 1;
+                v += 1;
+            }
+        }
+    }
+
+    public static int solve(MyGraph g) {
+        System.out.print("|V|: " + g.size() + " || ");
+        System.out.print("|E|: " + g.getEdgeCount() + " || ");
+        int size = g.size();
+        for (int i = 1; i < size; i++) {
+            Instance in = new Instance(g,i);
+            if (solve(in)) {
+                System.out.println("k: " + i);
+                return i;
+            }
+        }
+        System.out.println("k: 0");
+        return 0;
+    }
+
+}
